f579faa6bc6ae943c4920fdd5a91e1ac78bc1d07
mao <1296193245@qq.com>
2024-03-19 14:22:13 +0800

    提交

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,40 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, {RouteConfig} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
+const list = [];
+
+for (let i = 0; i < 10; i++)
+{
+    list.push({
+        path: '/view' + (i + 1),
+        name: '/view' + (i + 1),
+        component: () => import( '../views/View' + (i + 1) + '.vue')
+    },)
+}
+
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    ...list
+
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/View1.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="container"></div>
+</template>
+
+<script>
+import {initAMap, destroy} from '../utils/amapUtils'
+
+export default {
+    name: "View1",
+    data()
+    {
+        return {
+            map: null,
+        }
+    },
+    methods: {},
+    created()
+    {
+        console.log("初始化")
+        this.map = initAMap();
+    },
+    beforeDestroy()
+    {
+        console.log("销毁")
+        destroy(this.map)
+    },
+}
+</script>
+
+<style scoped>
+#container {
+    padding: 0px;
+    margin: 0px;
+    width: 100%;
+    height: 800px;
+}
+</style>
